44ebac01738d29302d10394e416a781b2f42708b
Thi Nguyễn <nguyenthisc1@gmail.com>
2024-12-21 19:24:29 +0700

    feat: drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'drizzle-kit'
+
+import { config } from "dotenv";
+import { defineConfig } from 'drizzle-kit';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+config({ path: ".env.local" });
 
 export default defineConfig({
     schema: './src/configs/schema.ts',
     out: './drizzle',
     dialect: 'postgresql',
     dbCredentials: {
-        url: 'postgresql://neondb_owner:4bigsFamML2O@ep-broad-forest-a4kt3s8t.us-east-1.aws.neon.tech/neondb?sslmode=require',
+        url: process.env.NEXT_PUBLIC_DATABASE_URL_CONFIG!,
     },
 })
